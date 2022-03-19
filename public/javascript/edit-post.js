0f452ed988c2fbf57b1113305a372e8af5f12daa
CRUMBB <aanyahlc@gmail.com>
2022-03-19 15:19:52 -0400

    starting code for all files/css creation and design

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,14 +1,21 @@
-async function deleteFormHandler(event) {
+async function editFormHandler(event) {
     event.preventDefault();
+
+    const title = document.querySelector('input[name="post-title"]').value.trim();
+    const content = document.querySelector('input[name="content"]').value.trim();
+    console.log(title);
+    console.log(content);
 
     const id = window.location.toString().split('/')[
         window.location.toString().split('/').length - 1
     ];
 
     const response = await fetch(`/api/posts/${id}`, {
-        method: 'DELETE',
+        method: 'PUT',
         body: JSON.stringify({
-            post_id: id
+            post_id: id,
+            title,
+            content
         }),
         headers: {
             'Content-Type': 'application/json'
@@ -23,4 +30,4 @@ async function deleteFormHandler(event) {
 
 }
 
-document.querySelector('.delete-post-btn').addEventListener('click', deleteFormHandler);
+document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
